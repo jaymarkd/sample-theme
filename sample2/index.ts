babafc53d1c48b736dcf0fe3db9877d82757e4a9
Jay Mark David <jaymarkd@cloudstaff.com>
2021-06-18 13:23:38 +0800

    Sample 2

---
+++ sample2/index.ts
@@ -0,0 +1,10 @@
+// start:ng42.barrel
+export * from './login';
+export * from './login/messages';
+export * from './login/resources';
+export * from './login/resources/css';
+export * from './login/resources/img';
+export * from './login/resources/js';
+export * from './login/resources/js/polyfill';
+// end:ng42.barrel
+
